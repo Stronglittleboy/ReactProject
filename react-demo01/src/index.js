a10d13785a4e504191f3547bf094a2dbc5c1f4e4
houlongwang <houlongwang@51jk.com>
2020-11-04 14:52:18 +0800

    1.组合组件

--- react-demo01/src/index.js
+++ react-demo01/src/index.js
@@ -4,9 +4,18 @@ import './index.css';
 function Welcome(props) {
     return <h1>Hello, {props.name}</h1>;
 }
+function App(){
+    return (
+        <div>
+            <Welcome name="李白"/>
+            <Welcome name="杜甫"/>
+            <Welcome name="王一帆"/>
+        </div>
+    );
+}
 
 const element = <Welcome name="Sara" />;
 ReactDOM.render(
-    element,
+    <App/>,
     document.getElementById('root')
 );
